ceffe5e97220b5d2e3ef4f09f8c48ae066316f11
M.Mahdavi <mahdavi@sahab.ir>
2024-01-22 12:08:56 +0330

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    2
+    <slot />
+    3
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    home
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
