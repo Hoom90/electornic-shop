d9782d41407abfc3740e5b2bc2d55bbfa636efe8
M.Mahdavi <mahdavi@sahab.ir>
2024-01-30 17:59:01 +0330

    redirect to home add from login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  routeRules:{
+    '/auth/forgetpass':{redirect:'/'}
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
